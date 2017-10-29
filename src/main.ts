be8f4a620da89b21a1d39e8b0103ac16ad7a4904
Christopher Gonzalez <amlidoscg@gmail.com>
2017-10-29 10:03:21 -0500

    d

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
-}
+} //commit
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
