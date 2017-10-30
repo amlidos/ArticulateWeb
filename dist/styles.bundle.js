1cf0a16a45e2c7d0968f982d3790133ff7a5b2df
Christopher Gonzalez <amlidoscg@gmail.com>
2017-10-30 16:33:40 -0500

    config 2

--- dist/styles.bundle.js
+++ dist/styles.bundle.js
@@ -35,7 +35,7 @@ exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-b
 exports.i(__webpack_require__("../../../../css-loader/index.js?{\"sourceMap\":false,\"importLoaders\":1}!../../../../postcss-loader/index.js?{\"ident\":\"postcss\"}!../../../material/prebuilt-themes/deeppurple-amber.css"), "");
 
 // module
-exports.push([module.i, "body {\n    background: \"./assets/1511.jpg\";\n}", ""]);
+exports.push([module.i, "body {\r\n    background: \"./assets/1511.jpg\";\r\n}", ""]);
 
 // exports
 
